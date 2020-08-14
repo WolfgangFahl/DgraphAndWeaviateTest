b6af48cdbb446a17bbd725ecdb8944efaf57a906
Wolfgang Fahl <wf@bitplan.com>
2020-08-14 16:33:03 +0200

    adds local blazegraph test

--- tests/testJena.py
+++ tests/testJena.py
@@ -4,7 +4,9 @@ Created on 2020-08-14
 @author: wf
 '''
 import unittest
+import getpass
 from dg.jena import Jena
+
 
 class TestJena(unittest.TestCase):
 
@@ -27,6 +29,27 @@ class TestJena(unittest.TestCase):
         results=jena.query(queryString)
         self.assertEqual(20,len(results))
         pass
+    
+    def testLocalWikdata(self):
+        '''
+        check local wikidata
+        '''
+        # check we have local wikidata copy:
+        if getpass.getuser()=="wf":
+            # use 2018 wikidata copy
+            endpoint="http://blazegraph.bitplan.com/sparql"
+            jena=Jena(endpoint)
+            queryString="""
+            SELECT ?item ?coord 
+WHERE 
+{
+  # instance of whisky distillery
+  ?item wdt:P31 wd:Q10373548.
+  # get the coordindate
+  ?item wdt:P625 ?coord.
+}"""
+            results=jena.query(queryString)
+            self.assertEqual(238,len(results))
 
 
 if __name__ == "__main__":
